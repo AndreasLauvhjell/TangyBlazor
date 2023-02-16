fe80542f8b93ca5df0652b10dd811cc6b708f476
Hjaldrgud <lauvhjell@gmail.com>
2023-02-16 11:29:45 +0100

    Litt videre

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -10,6 +10,8 @@ namespace Tangy_Models.LearnBlazorModels
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public double Price { get; set; }
         public bool IsActive { get; set; }
+
     }
 }
