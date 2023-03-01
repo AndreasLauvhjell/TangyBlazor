80127818e1c1d8431abb0108013460fd3aefd60b
Hjaldrgud <lauvhjell@gmail.com>
2023-03-01 22:48:46 +0100

    har kommet til 3.25

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -13,5 +13,6 @@ namespace Tangy_Models.LearnBlazorModels
         public double Price { get; set; }
         public bool IsActive { get; set; }
 
+        public List<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
