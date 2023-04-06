baa5748427a9e40882a236c23c1d4053084351b1
Hjaldrgud <lauvhjell@gmail.com>
2023-04-06 19:52:24 +0200

    b

--- TangyWeb_Client/Service/ProductService.cs
+++ TangyWeb_Client/Service/ProductService.cs
@@ -7,9 +7,14 @@ namespace TangyWeb_Client.Service
     public class ProductService : IProductService
     {
         private readonly HttpClient _httpClient;
-        public ProductService(HttpClient httpClient)
+        private IConfiguration _configuration;
+        private string BaseServerUrl;
+
+        public ProductService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
+            BaseServerUrl = _configuration.GetSection("BaseServerUrl").Value;
         }
 
         public async Task<ProductDTO> Get(int productId)
@@ -19,6 +24,7 @@ namespace TangyWeb_Client.Service
             if (response.IsSuccessStatusCode)
             {
                 var product = JsonConvert.DeserializeObject<ProductDTO>(content);
+                product.ImageUrl = BaseServerUrl + product.ImageUrl;
                 return product;
             }
             else
@@ -35,6 +41,10 @@ namespace TangyWeb_Client.Service
             {
                 var content = await response.Content.ReadAsStringAsync();
                 var products = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(content);
+                foreach(var prod in products)
+                {
+                    prod.ImageUrl = BaseServerUrl + prod.ImageUrl;
+                }
                 return products;
             }
             else
