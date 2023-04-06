2750c0a8aeb4612f28f8931d0bc3316b48b07114
Hjaldrgud <lauvhjell@gmail.com>
2023-04-06 19:06:18 +0200

    StepByStep

--- TangyWeb_ClientNEW/Program.cs
+++ TangyWeb_ClientNEW/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("BaseAPIUrl"))});
-builder.
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("BaseAPIUrl")) });
+builder.Services.AddScoped<IProductService, IProductService>();
 
 await builder.Build().RunAsync();
--- TangyWeb_ClientNEW/Service/IService/IProductService.cs
+++ TangyWeb_ClientNEW/Service/IService/IProductService.cs
@@ -4,7 +4,7 @@ namespace TangyWeb_Client.Service.IService
 {
     public interface IProductService
     {
-        //public Task<IEnumerable<ProductDTO>> GetAll();
-        //public Task<ProductDTO> Get(int productId);
+        public Task<IEnumerable<ProductDTO>> GetAll();
+        public Task<ProductDTO> Get(int productId);
     }
 }
