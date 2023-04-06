cbdc54353536eed4d18018458136186a5994516b
Hjaldrgud <lauvhjell@gmail.com>
2023-04-06 19:11:09 +0200

    b

--- TangyWeb_Client/Service/ProductService.cs
+++ TangyWeb_Client/Service/ProductService.cs
@@ -5,6 +5,12 @@ namespace TangyWeb_Client.Service
 {
     public class ProductService : IProductService
     {
+        private readonly HttpClient _httpClient;
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public Task<ProductDTO> Get(int productId)
         {
             throw new NotImplementedException();
