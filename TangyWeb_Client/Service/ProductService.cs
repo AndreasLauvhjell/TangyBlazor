0638acdde24cdf3731225cbbcf694ec9b5b282ce
Hjaldrgud <lauvhjell@gmail.com>
2023-04-06 19:28:49 +0200

    b

--- TangyWeb_Client/Service/ProductService.cs
+++ TangyWeb_Client/Service/ProductService.cs
@@ -14,7 +14,7 @@ namespace TangyWeb_Client.Service
 
         public async Task<ProductDTO> Get(int productId)
         {
-            var response = await _httpClient.GetAsync($"/api/products/{productId}");
+            var response = await _httpClient.GetAsync($"/api/product/{productId}");
             var content = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
