401e1dbd5b33192c6130c64df70e707b893f9d8d
Hjaldrgud <lauvhjell@gmail.com>
2023-04-15 23:50:15 +0200

    Ferdig med seksjon 16

--- Tangy_DataAccess/OrderHeader.cs
+++ Tangy_DataAccess/OrderHeader.cs
@@ -17,13 +17,11 @@ namespace Tangy_DataAccess
         //Add navigation property #TODO
 
         [Required]
-        [Display(Name = "Order Total")]
         public double OrderTotal { get; set; }
 
         [Required]
         public DateTime OrderDate { get; set; }
         [Required]
-        [Display(Name = "Shipping Date")]
         public DateTime ShippingDate { get; set; }
 
         public string Status { get; set; }
@@ -32,22 +30,19 @@ namespace Tangy_DataAccess
         public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
-        [Display(Name = "Name")]
         [Required]
         public string Name { get; set; }
         [Required]
-        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         [Required]
-        [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
         [Required]
         public string State { get; set; }
         [Required]
         public string City { get; set; }
         [Required]
-        [Display(Name = "PostalCode")]
         public string PostalCode { get; set; }
 
+        public string Email { get; set; }
     }
 }
