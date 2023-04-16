6401d8cf0ceb48589103e24b05915393d2f2388d
Hjaldrgud <lauvhjell@gmail.com>
2023-04-16 23:15:20 +0200

    Starta på Seksjon17. Veldig tungt. Mye rart for å få fiksa Identity. Scaffold wizard fungerte ikke, og jeg måtte manuelt installere Identity gjennom commands. Fikk også standardisert .NET versjonen til 6.0.13 som en del av mine forsøk på å få scaffold-wizarden til å fungere.

--- TangyWeb_Server/Program.cs
+++ TangyWeb_Server/Program.cs
@@ -8,6 +8,7 @@ using Tangy_DataAccess.Data;
 using TangyWeb_Server.Data;
 using TangyWeb_Server.Service;
 using TangyWeb_Server.Service.IService;
+using Microsoft.AspNetCore.Identity;
 
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTUyNDA5MEAzMjMxMmUzMTJlMzMzNW9BT0FLblNwNWxYblU2ZG5nRlpWMU9MN0JwQWxSZDkvaTBNUEJsWllJaFU9;Mgo+DSMBaFt+QHFqVkNrWE5AaV1CX2BZell1TWlbd04QCV5EYF5SRHVdSlxlSnZRfkBhXHo=;Mgo+DSMBMAY9C3t2VFhhQlJBfVtdWXxLflF1VWdTfFZ6dl1WACFaRnZdQV1gS3ZScEZrW3dbdHVQ;Mgo+DSMBPh8sVXJ1S0d+X1RPckBGQmFJfFBmQWlad1RyfEUmHVdTRHRcQl5hQX9VdU1hUXpbd3E=;MTUyNDA5NEAzMjMxMmUzMTJlMzMzNWRMNkw3OFpzR0V0UXlwaEdVWU92RitjR1RUbUMxeUFuQjFKTU1WWlhvaTg9;NRAiBiAaIQQuGjN/V0d+XU9Hc1RGQmdWfFN0RnNddV14fldOcDwsT3RfQF5jSnxbd0NgUX1WdX1TQw==;ORg4AjUWIQA/Gnt2VFhhQlJBfVtdWXxLflF1VWdTfFZ6dl1WACFaRnZdQV1gS3ZScEZrW3dYcXVQ;MTUyNDA5N0AzMjMxMmUzMTJlMzMzNURzUUJCMWJNRkh6aktNekNPT25HRDdGVENENU4vdCtwT2lLanRxTm5MNE09;MTUyNDA5OEAzMjMxMmUzMTJlMzMzNWF6YjlWMGZlNUtBdlBMUVJMR3F5L2ZuaksyWFp3cGt1UWIwV1V6MlNvK009;MTUyNDA5OUAzMjMxMmUzMTJlMzMzNUVZSmg5cXo0amhRNnMrSHovVURIUGZ3TFppemhpd0lWcElkbFhxRGJoRkU9;MTUyNDEwMEAzMjMxMmUzMTJlMzMzNVJwblZnSFB2WjJzYTAzK0cwNDBYNCtuQjdoblMwTlg0bnJtUmRMU3l5TDg9;MTUyNDEwMUAzMjMxMmUzMTJlMzMzNW9BT0FLblNwNWxYblU2ZG5nRlpWMU9MN0JwQWxSZDkvaTBNUEJsWllJaFU9 ");
 
@@ -20,6 +21,8 @@ builder.Services.AddSyncfusionBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductPriceRepository, ProductPriceRepository>();
@@ -43,5 +46,6 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.UseAuthentication();;
 
 app.Run();
