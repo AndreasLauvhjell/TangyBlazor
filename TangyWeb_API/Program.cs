ad6408b0d1484da23a1b959ee83acf721b459060
Hjaldrgud <lauvhjell@gmail.com>
2023-07-15 17:43:13 +0200

    Starta på Seksjon 18

--- TangyWeb_API/Program.cs
+++ TangyWeb_API/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Tangy_Business.Repository.IRepository;
 using Tangy_Business.Repository;
 using Tangy_DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+using Tangy_DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
---
+++ Tangy_Models/SignupRequestDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class SignupRequestDTO
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+
+        [RegularExpression("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", ErrorMessage = "Invalid Email Format")]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
+        public string ConfirmPassword { get; set;}
+    }
+}
