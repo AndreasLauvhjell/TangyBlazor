bb1a58354929c3309497b955d56d445a34162379
Hjaldrgud <lauvhjell@gmail.com>
2023-07-17 18:05:33 +0200

    Ferdig med seksjon 18

--- TangyWeb_API/Controllers/AccountController.cs
+++ TangyWeb_API/Controllers/AccountController.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Tangy_Common;
 using Tangy_DataAccess;
 using Tangy_Models;
+using TangyWeb_API.Helper;
 
 namespace TangyWeb_API.Controllers
 {
@@ -12,21 +18,24 @@ namespace TangyWeb_API.Controllers
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly APISettings _apiSettings;
 
         public AccountController(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager,
+            IOptions<APISettings> options)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
+            _apiSettings = options.Value;
         }
 
         [HttpPost]
         public async Task<IActionResult> SignUp([FromBody] SignupRequestDTO signupRequestDTO)
         {
-            if(signupRequestDTO== null || !ModelState.IsValid) 
+            if (signupRequestDTO == null || !ModelState.IsValid)
             {
                 return BadRequest();
             }
@@ -42,7 +51,7 @@ namespace TangyWeb_API.Controllers
 
             var result = await _userManager.CreateAsync(user, signupRequestDTO.Password);
 
-            if(!result.Succeeded)
+            if (!result.Succeeded)
             {
                 return BadRequest(new SignUpResponseDTO()
                 {
@@ -62,6 +71,90 @@ namespace TangyWeb_API.Controllers
             }
 
             return StatusCode(201);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn([FromBody] SignInRequestDTO signInRequestDTO)
+        {
+            if (signInRequestDTO == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+
+            var result = await _signInManager.PasswordSignInAsync(signInRequestDTO.Username, signInRequestDTO.Password, false, false);
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(signInRequestDTO.Username);
+                if (user == null)
+                {
+                    return Unauthorized(new SignInResponseDTO()
+                    {
+                        IsAuthSuccessful = false,
+                        ErrorMessage = "Invalid Authentication"
+                    });
+                }
+                //Everything is valid and we need to login
+                var signinCredentials = GetSigningCredentials();
+                var claims = await GetClaims(user);
+
+                var tokenOption = new JwtSecurityToken(
+                    issuer: _apiSettings.ValidIssuer,
+                    audience: _apiSettings.ValidAudience,
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(30),
+                    signingCredentials: signinCredentials);
+
+                var token = new JwtSecurityTokenHandler().WriteToken(tokenOption);
+
+                return Ok(new SignInResponseDTO()
+                {
+                    IsAuthSuccessful = true,
+                    Token = token,
+                    UserDTO = new UserDTO()
+                    {
+                        Name = user.Name,
+                        Email = user.Email,
+                        Id = user.Id,
+                        PhoneNumber = user.PhoneNumber
+                    }
+                });
+            }
+            else
+            {
+                return Unauthorized(new SignInResponseDTO()
+                {
+                    IsAuthSuccessful = false,
+                    ErrorMessage = "Invalid Authentication"
+                });
+            }
+
+            return StatusCode(201);
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_apiSettings.SecretKey));
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim("id", user.Id),
+            };
+
+            var roles = await _userManager.GetRolesAsync(await _userManager.FindByEmailAsync(user.Email));
+            foreach(var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
         }
     }
 }
